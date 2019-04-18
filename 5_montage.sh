9b8309d7716af903c2427a200d9ac173f89a2368
alykat <ahurt@npr.org>
2019-04-18 13:50:36 -0400

    update montage script

--- 5_montage.sh
+++ 5_montage.sh
@@ -18,13 +18,18 @@ function makeMontage() {
     files+=("pages/report-$page.png")
   done
   # echo "${files[@]}";
-  montage "${files[@]}" -geometry 100x -tile 5x "montages/tiled-$name.png"
+  montage "${files[@]}" -geometry 100x -quality 60% -tile 10x "montages/tiled-$name.png"
 }
 
-makeMontage 22 43 section1-II
-makeMontage 44 73 section1-III
-makeMontage 74 181 section1-IV
-makeMontage 182 207 section1-V
-makeMontage 222 227 section2-I
-makeMontage 228 371 section2-II
-makeMontage 372 394 section2-III
+makeMontage 9 18 section1-summary
+makeMontage 19 21 section1-i
+makeMontage 22 43 section1-ii
+makeMontage 44 73 section1-iii
+makeMontage 74 181 section1-iv
+makeMontage 182 207 section1-v
+makeMontage 213 220 section2-summary
+makeMontage 221 226 section2-i
+makeMontage 227 370 section2-ii
+makeMontage 371 393 section2-iii
+makeMontage 394 394 section2-iv
+makeMontage 395 448 appendices
